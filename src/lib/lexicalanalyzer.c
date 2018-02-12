e9feedb3f0583aedffb8176dca61492fe0517600
victorsndvg <victorsv@gmail.com>
2018-02-12 19:15:49 +0100

    Change initialization to the main file. Fix #10

--- src/lib/lexicalanalyzer.c
+++ src/lib/lexicalanalyzer.c
@@ -6,47 +6,6 @@
 #include <dictionary.h>
 #include <definitions.h>
 
-//Inicializa la tabla de simbolos con las palabras reservadas
-void lex_initialize(){
-    push("package", _PACKAGE);
-    push("import", _IMPORT);
-    push("func", _FUNC);
-    push("var", _VAR);
-    push("chan", _CHAN);
-    push("for", _FOR);
-    push("range", _RANGE);
-    push("range", _RANGE);
-    push("real", _REAL);
-    push("make", _MAKE);
-    push("go", _GO);
-    push("len", _LEN);
-    push("float32", _FLOAT32);
-    push("sum", _SUM);
-    push("==", _EQCOMP);
-    push(":=", _ASSIGN);
-    push("+=", _INCREASE);
-    push("<-", _SEND);
-    push("(", _OP);
-    push(")", _CP);
-    push("[", _OB);
-    push("]", _CP);
-    push("{", _OCB);
-    push("}", _CCB);
-    push(",", _COMMA);
-    push(":", _C);
-    push(";", _SC);
-    push("/", _DIV);
-    push("=", _EQ);
-    push("+", _SUMM);
-    push("-", _MINUS);
-    push(".", _POINT);
-}
-
-//Inicializa el analizador lexico
-void lex_create(int chunk, FILE *file_ptr) {
-    doublebuffer_create(chunk, file_ptr);
-	lex_initialize();
-}
 
 //Devuelve el proximo caracter desde el doble buffer
 char lex_nextchar() {
